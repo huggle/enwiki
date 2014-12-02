c58a849a198cfa9fac35106a7264e7d1ddacf90e
Petr Bena <benapetr@gmail.com>
2014-12-02 10:45:44 +0100

    code

---
+++ src/enwiki.cpp
@@ -0,0 +1,78 @@
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+#include "enwiki.h"
+#include <core.hpp>
+#include <collectable_smartptr.hpp>
+#include <localization.hpp>
+#include <query.hpp>
+#include <querypool.hpp>
+#include <mainwindow.hpp>
+#include <generic.hpp>
+#include <ui_mainwindow.h>
+#include <wikiedit.hpp>
+#include <wikisite.hpp>
+#include <wikipage.hpp>
+#include <syslog.hpp>
+#include <configuration.hpp>
+
+using namespace Huggle;
+
+enwiki::enwiki()
+{
+}
+
+bool enwiki::Register()
+{
+    Huggle::Core::HuggleCore = (Huggle::Core*) this->HuggleCore;
+    Huggle::QueryPool::HugglePool = Huggle::Core::HuggleCore->HGQP;
+    Huggle::Localizations::HuggleLocalizations = (Huggle::Localizations*) this->Localization;
+    Huggle::Syslog::HuggleLogs = Huggle::Core::HuggleCore->HuggleSyslog;
+    Huggle::GC::gc = Huggle::Core::HuggleCore->gc;
+    Huggle::Query::NetworkManager = this->Networking;
+    Huggle::Configuration::HuggleConfiguration = (Huggle::Configuration*) this->Configuration;
+    return true;
+}
+
+bool enwiki::IsWorking()
+{
+    return true;
+}
+
+void enwiki::Hook_MainWindowOnLoad(void *window)
+{
+    this->Window = (Huggle::MainWindow*)window;
+    this->menuProd = new QAction("PROD", this->Window->ui->menuPage);
+    this->Window->ui->menuPage->addAction(this->menuProd);
+    connect(this->menuProd, SIGNAL(triggered()), this, SLOT(ClickPROD()));
+}
+
+void enwiki::ClickPROD()
+{
+    if (!this->Window->CheckEditableBrowserPage())
+        return;
+    if (this->Window->GetCurrentWikiEdit()->Page->IsTalk())
+    {
+        Generic::pMessageBox(this->Window, "Error", "You can't PROD talk pages");
+        return;
+    }
+    QString pn = this->Window->CurrentEdit.GetPtr()->Page->GetNS()->GetCanonicalName();
+    if (pn == "Template")
+    {
+        Generic::MessageBox("Error", "You can't use PROD for templates or categories");
+        return;
+    }
+}
+
+#if QT_VERSION < 0x05000
+    Q_EXPORT_PLUGIN2("org.huggle.extension.qt", enwiki)
+#endif
+
+
---
+++ src/enwiki.h
@@ -0,0 +1,54 @@
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+#ifndef ENWIKI_H
+#define ENWIKI_H
+
+#define HUGGLE_EXTENSION
+#include <iextension.hpp>
+#include <QAction>
+#include <QDeclarativeItem>
+
+namespace Huggle
+{
+    class MainWindow;
+}
+
+class enwiki : public QDeclarativeItem, public Huggle::iExtension
+{
+    Q_OBJECT
+#if QT_VERSION >= 0x05000
+        Q_PLUGIN_METADATA(IID "org.huggle.extension.qt" FILE "enwiki.json")
+#endif
+    Q_INTERFACES(Huggle::iExtension)
+    public:
+        enwiki();
+        bool Register();
+        bool IsWorking();
+        void Hook_MainWindowOnLoad(void *window);
+        QString GetExtensionName() { return "English wikipedia tools"; }
+        QString GetExtensionAuthor() { return "Petr Bena"; }
+        QString GetExtensionDescription() { return "Various tools for english wikipedia"; }
+        QString GetExtensionVersion() { return "1.0.0"; }
+        QAction *menu;
+        bool RequestCore() { return true; }
+        bool RequestNetwork() { return true; }
+        bool RequestConfiguration() { return true; }
+    private slots:
+        void ClickPROD();
+    private:
+        QAction *menuProd = nullptr;
+        QAction *menuAfd = nullptr;
+        Huggle::MainWindow *Window;
+};
+
+QML_DECLARE_TYPE(enwiki)
+
+#endif // ENWIKI_H
