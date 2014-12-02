a9a801ab1c47ee58f33ddfc9c60bc52c4a591ae5
Petr Bena <benapetr@gmail.com>
2014-12-02 10:45:44 +0100

    code

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
-enwiki
-======
+mass-delivery
+=============
 
-Plugin for huggle3 that contains tools for english wikipedia
+The mass-delivery extension allows a user to send message to a list of users.
+
+Note: This extension should be checked out in the folder named "huggle" in your huggle3-qt-lx repo
+
+How to build
+=============
+
+Prepare a directory for build, in this example it's /tmp/huggle
+
+```
+cd /tmp/huggle
+git clone http://github.com/huggle/mass-delivery
+git clone http://github.com/huggle/huggle3-qt-lx
+# build core
+cd huggle3-qt-lx/huggle
+./configure
+make
+cd -
+cd mass-delivery-md
+qmake
+make
+# copy the plugin to extensions dir
+cp libMassDelivery.so ../..
+cd -
+cd huggle3-qt-lx/huggle
+mkdir extensions
+mv ../../libMassDelivery.so extensions
+```
+
+Now you should have the mass-delivery extension installed in Huggle
---
+++ src/enwiki.cpp
@@ -0,0 +1,44 @@
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+#include "enwiki.h"
+#include <core.hpp>
+#include <localization.hpp>
+#include <query.hpp>
+#include <querypool.hpp>
+#include <syslog.hpp>
+#include <configuration.hpp>
+
+enwiki::enwiki()
+{
+}
+
+bool enwiki::Register()
+{
+    Huggle::Core::HuggleCore = (Huggle::Core*) this->HuggleCore;
+    Huggle::QueryPool::HugglePool = Huggle::Core::HuggleCore->HGQP;
+    Huggle::Localizations::HuggleLocalizations = (Huggle::Localizations*) this->Localization;
+    Huggle::Syslog::HuggleLogs = Huggle::Core::HuggleCore->HuggleSyslog;
+    Huggle::GC::gc = Huggle::Core::HuggleCore->gc;
+    Huggle::Query::NetworkManager = this->Networking;
+    Huggle::Configuration::HuggleConfiguration = (Huggle::Configuration*) this->Configuration;
+    return true;
+}
+
+bool enwiki::IsWorking()
+{
+    return true;
+}
+
+#if QT_VERSION < 0x05000
+    Q_EXPORT_PLUGIN2("org.huggle.extension.qt", enwiki)
+#endif
+
+
